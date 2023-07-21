9208ca8bb0e584dff3342254623ced23af246710
mahesh855 <maheshgoud0305@gmail.com>
2023-07-21 13:35:01 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Home from './components/Home';
 import UserLogin from './components/login/UserLogin';
 import Dashboard from './components/Dashboard/Dashboard';
+import AdminDashboard from './components/Dashboard/AdminDashboard';
+import { ProjectDetails } from './components/Dashboard/ProjectDetails';
 
 // import UserDashboard from './components/Userdashboard/UserDashboard';
 
@@ -17,6 +18,10 @@ function App() {
         <Route path='/' exact element={<Home/>}/>
         <Route path='/Dashboard' exact element={<Dashboard/>}/>
         <Route path='/UserLogin' exact element={<UserLogin/>}/>
+        <Route path='/AdminDashboard' exact element={<AdminDashboard/>}/>
+        <Route path='/ProjectDetails' exact element={<ProjectDetails/>}/>
+
+
       </Routes>
       </BrowserRouter>
     </div>
